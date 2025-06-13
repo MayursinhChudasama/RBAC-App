9ed166878ee22524426990f7101562fc01f4ea00
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-06-13 11:14:36 +0530

    Initialize React Redux Toolkit project with Vite, Tailwind CSS, and ESLint setup. Add main application structure and styling.

--- scripts/closeModal.js
+++
@@ -1,7 +0,0 @@
-import { sidebar } from "./handleCurrentTab.js";
-import { modal, navbar } from "./openModal.js";
-export function closeModal() {
-  modal.classList.remove("open-modal");
-  sidebar.classList.remove("disabled");
-  navbar.classList.remove("disabled");
-}
--- scripts/fetchComponent.js
+++
@@ -1,5 +0,0 @@
-export async function fetchComponent(url, element) {
-  const response = await fetch(url);
-  const html = await response.text();
-  element.innerHTML = html;
-}
