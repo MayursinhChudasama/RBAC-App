64b9621ff9519038bcfe22322ccdc23aecfcb6ea
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-07-16 17:17:02 +0530

    starting : RBAC App with TypeScript and React

---
+++ TypeScript React/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn mor
+      </p>
+    </>
+  )
+}
+
+export default App
