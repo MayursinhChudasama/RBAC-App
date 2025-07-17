85518a68b0cf7fd95bbf452316960537984ce3fe
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-07-17 18:20:54 +0530

    RBAC App: added react-router-dom, redux-toolkit packages component: Login

---
+++ TypeScript React/Pages/LoginPage.tsx
@@ -0,0 +1,71 @@
+import { useRef, useState } from "react";
+// import { useFetchDataQuery } from "../store/dataApiSlice";
+import { useNavigate } from "react-router-dom";
+
+import Login from "../components/Login";
+export default function LoginPage() {
+  // localStorage.removeItem("user");
+  const [user, setUser] = useState(false);
+  const [err, setErr] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const navigate = useNavigate();
+
+  const { data, isLoading, isError, error } = useFetchDataQuery();
+  //
+  function handleChange() {
+    console.log(emailRef.current.value);
+  }
+  //
+  function handleEmail() {
+    console.log(emailRef.current.value);
+    const inputEmail = emailRef.current.value;
+    const inputUser = data.users.find((user) => user.email === inputEmail);
+    if (inputUser) {
+      setErr(false);
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ email: inputUser.email, name: inputUser.name })
+      );
+      const inputUserEmail = JSON.parse(localStorage.getItem("user"));
+      setUser(inputUserEmail);
+      console.log("email done");
+      return;
+    }
+    setErr(true);
+    console.log("email not found");
+
+    return;
+  }
+  console.log(user);
+
+  function handlePassword() {
+    // if (user?.email) {
+    setErr(false);
+    console.log("email entered, please enter password");
+    const inputPassword = passwordRef.current.value;
+    const inputUser = data.users.find(
+      (user) => user.password === inputPassword
+    );
+    if (inputUser) {
+      localStorage.setItem("user", JSON.stringify(inputUser));
+      console.log(
+        "successfully logged in:",
+        JSON.parse(localStorage.getItem("user"))
+      );
+      const inputUserPass = JSON.parse(localStorage.getItem("user"));
+      setUser(inputUserPass);
+      navigate("/home");
+      return;
+    }
+    setErr(true);
+    // }
+    return;
+  }
+  //
+  return (
+    <Login
+      obj={{ emailRef, passwordRef, handlePassword, handleEmail, user, err }}
+    />
+  );
+}
--- TypeScript React/src/App.tsx
+++ TypeScript React/src/App.tsx
@@ -1,19 +1,47 @@
-
-
-
-
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Navigate
+          to='/login'
+          replace
+        />
+      ),
+    },
+    {
+      path: "login",
+      element: <LoginPage />,
+    },
+    {
+      path: "home",
+      element: <RootLayout />,
+      children: [
+        {
+          path: ":page",
+          element: <Content />,
+          children: [
+            {
+              index: true,
+              element: <TypePage />,
+            },
+            {
+              path: "new",
+              element: <NewEntry />,
+            },
+            {
+              path: "edit/:id",
+              element: <NewEntry />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-
-  return (
-    <>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn mor
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
